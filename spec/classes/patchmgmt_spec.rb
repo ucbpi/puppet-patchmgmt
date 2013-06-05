263d8e75140466a82eec8b01a2bf24a019d7f49a
Aaron Russo <arusso@berkeley.edu>
2013-06-05 12:08:14 -0700

    adding tests to ensure email changes work, patch/clean disabling works

--- spec/classes/patchmgmt_spec.rb
+++ spec/classes/patchmgmt_spec.rb
@@ -47,4 +47,117 @@ describe 'patchmgmt' do
       should contain_file('/usr/local/sbin/print_enabled_repos')
     end
   end
+
+  context "=> change target email addresses" do
+    let(:params) {
+      { 'patch_day' => 'wed',
+        'patch_hour' => '10',
+        'patch_email' => 'patch-notify@localhost',
+        'list_day' => 'tue',
+        'list_hour' => '10',
+        'list_email' => 'list-notify@localhost',
+        'clean_day' => 'mon',
+        'clean_hour' => '10',
+        'clean_email' => 'clean-notify@localhost' } }
+    it do
+      should contain_file('/usr/local/sbin/runupdates') \
+        .with_content(/TARGET='patch-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/listupdates') \
+        .with_content(/TARGET='list-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/cleanupdates') \
+        .with_content(/TARGET='clean-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/) 
+                      
+      should contain_file('/etc/patchmgmt.conf') \
+        .with_content(/MAKE_YUM_CACHE="yes"/) \
+        .with_content(/RUN_YUM_UPDATES="yes"/)
+
+      should contain_file('/etc/cron.d/patchmgmt') \
+        .with_content(/0 10 \* \* mon root \/usr\/local\/sbin\/cleanupdates/) \
+        .with_content(/0 10 \* \* tue root \/usr\/local\/sbin\/listupdates/) \
+        .with_content(/0 10 \* \* wed root \/usr\/local\/sbin\/runupdates/)
+
+      should contain_file('/usr/local/sbin/print_enabled_repos')
+    end
+  end
+
+  context "=> disable patching" do
+    let(:params) {
+      { 'patch_day' => 'wed',
+        'patch_hour' => '10',
+        'patch_email' => 'patch-notify@localhost',
+        'patch' => false,
+        'list_day' => 'tue',
+        'list_hour' => '10',
+        'list_email' => 'list-notify@localhost',
+        'clean_day' => 'mon',
+        'clean_hour' => '10',
+        'clean_email' => 'clean-notify@localhost' } }
+    it do
+      should contain_file('/usr/local/sbin/runupdates') \
+        .with_content(/TARGET='patch-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/listupdates') \
+        .with_content(/TARGET='list-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/cleanupdates') \
+        .with_content(/TARGET='clean-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/) 
+                      
+      should contain_file('/etc/patchmgmt.conf') \
+        .with_content(/MAKE_YUM_CACHE="yes"/) \
+        .with_content(/RUN_YUM_UPDATES="no"/)
+
+      should contain_file('/etc/cron.d/patchmgmt') \
+        .with_content(/0 10 \* \* mon root \/usr\/local\/sbin\/cleanupdates/) \
+        .with_content(/0 10 \* \* tue root \/usr\/local\/sbin\/listupdates/) \
+        .with_content(/0 10 \* \* wed root \/usr\/local\/sbin\/runupdates/)
+
+      should contain_file('/usr/local/sbin/print_enabled_repos')
+    end
+  end
+
+  context "=> disable cleaning" do
+    let(:params) {
+      { 'patch_day' => 'wed',
+        'patch_hour' => '10',
+        'patch_email' => 'patch-notify@localhost',
+        'list_day' => 'tue',
+        'list_hour' => '10',
+        'list_email' => 'list-notify@localhost',
+        'clean' => false,
+        'clean_day' => 'mon',
+        'clean_hour' => '10',
+        'clean_email' => 'clean-notify@localhost' } }
+    it do
+      should contain_file('/usr/local/sbin/runupdates') \
+        .with_content(/TARGET='patch-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/listupdates') \
+        .with_content(/TARGET='list-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/)
+
+      should contain_file('/usr/local/sbin/cleanupdates') \
+        .with_content(/TARGET='clean-notify@localhost'/) \
+        .with_content(/DELAY="-R 120"/) 
+                      
+      should contain_file('/etc/patchmgmt.conf') \
+        .with_content(/MAKE_YUM_CACHE="no"/) \
+        .with_content(/RUN_YUM_UPDATES="yes"/)
+
+      should contain_file('/etc/cron.d/patchmgmt') \
+        .with_content(/0 10 \* \* mon root \/usr\/local\/sbin\/cleanupdates/) \
+        .with_content(/0 10 \* \* tue root \/usr\/local\/sbin\/listupdates/) \
+        .with_content(/0 10 \* \* wed root \/usr\/local\/sbin\/runupdates/)
+
+      should contain_file('/usr/local/sbin/print_enabled_repos')
+    end
+  end
 end
